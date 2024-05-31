ebc83e818139576442ef29eeadb524c568281bde
Isaac <https://github.com/kenshinman21/wdd230.git>
2024-05-30 19:30:27 -0600

    made the code look a little better

--- scripts/chamber-site-plan.js
+++ scripts/chamber-site-plan.js
@@ -2,7 +2,7 @@ document.addEventListener('DOMContentLoaded', function () {
     document.getElementById('lastModified').textContent = 'Last modified: ' + document.lastModified;
 
     document.getElementById('homeButton').addEventListener('click', function() {
-        window.location.href = 'index.html'; // Directly navigates to the homepage
+        window.location.href = 'index.html'; 
     });
 
     document.querySelectorAll('.edit').forEach(button => {
@@ -10,18 +10,17 @@ document.addEventListener('DOMContentLoaded', function () {
             let paragraph = this.closest('.section').querySelector('p');
             if (paragraph.contentEditable === "true") {
                 paragraph.contentEditable = "false";
-                this.textContent = 'edit'; // Change button text to 'edit'
+                this.textContent = 'edit'; 
             } else {
                 paragraph.contentEditable = "true";
-                this.textContent = 'done'; // Change button text to 'done'
-                paragraph.focus(); // Focuses the paragraph to allow immediate editing
+                this.textContent = 'done'; 
+                paragraph.focus(); 
             }
         });
     });
 
     document.querySelectorAll('.edit-image').forEach(button => {
         button.addEventListener('click', function() {
-            // Trigger the hidden file input associated with this button
             const fileInput = this.nextElementSibling;
             fileInput.click();
         });
